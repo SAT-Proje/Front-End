74e9023fe729611d14fcdca50eac9221a17a2333
Bora ILCI <borailci@outlook.com>
2024-05-04 21:22:51 +0300

    Update .gitignore and package.json, refactor code structure, changed some routes

--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  user_id: { type: String, required: true },
+  user_id: { type: String, required: false }, // currently disabled
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  reservations: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reservation' }]
-});
+  reservations: [{ type: mongoose.Schema.Types.ObjectId, ref: "Reservation" }]
+})
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema)
 
-module.exports = User;
+module.exports = User
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/authController');
+const express = require("express")
+const router = express.Router()
+const AuthController = require("../controllers/authController")
 
 // Route for user registration
-router.post('/register', AuthController.register);
+router.post("/register", AuthController.register)
 
 // Route for user login
-router.post('/login', AuthController.login);
+router.post("/login", AuthController.login)
 
 // Route for user logout
-router.get('/logout', AuthController.logout);
+router.get("/logout", AuthController.logout)
 
-
-module.exports = router;
+module.exports = router
