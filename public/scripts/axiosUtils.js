ba826947db51b189038cd6a9ea958a6c3ac14b5d
Bora ILCI <borailci@outlook.com>
2024-05-06 20:02:58 +0300

    asdas

--- public/scripts/axiosUtils.js
+++ public/scripts/axiosUtils.js
@@ -1,103 +1,121 @@
-(function($global){
+;(function ($global) {
+  var axiosUtils = {}
+  axiosUtils.sendGetRequest = function (URL, handleResponse, handleError) {
+    axios
+      .get(URL)
+      .then(response => {
+        handleResponse(response)
+      })
+      .catch(error => {
+        handleError(error)
+      })
+  }
 
+  axiosUtils.sendPostRequest = function (
+    URL,
+    data,
+    handleResponse,
+    handleError
+  ) {
+    axios
+      .post(URL, data)
+      .then(response => {
+        handleResponse(response)
+      })
+      .catch(error => {
+        handleError(error)
+      })
+  }
 
-    var axiosUtils = {};
-    axiosUtils.sendGetRequest = function (URL, handleResponse, handleError){
-        axios.get(URL)
-        .then(response => {
-            handleResponse(response);
-        })
-        .catch(error => {
-            handleError(error);
-        });
+  axiosUtils.sendPutRequest = function (
+    URL,
+    data,
+    handleResponse,
+    handleError
+  ) {
+    axios
+      .put(URL, data)
+      .then(response => {
+        handleResponse(response)
+      })
+      .catch(error => {
+        handleError(error)
+      })
+  }
+
+  axiosUtils.sendDeleteRequest = function (URL, handleResponse, handleError) {
+    axios
+      .delete(URL)
+      .then(response => {
+        handleResponse(response)
+      })
+      .catch(error => {
+        handleError(error)
+      })
+  }
+
+  axiosUtils.loadLoggedInState = async function () {
+    try {
+      const response = await axios.get("/") // ???
+      const data = response.data
+      console.log(response)
+      const container = document.getElementById("login-signup-container")
+      if (data.loggedIn) {
+        const htmlResponse = await axios.get(
+          "./snippets/logged_in_snippet.html",
+          { responseType: "text" }
+        )
+        container.innerHTML = htmlResponse.data
+      } else {
+        const htmlResponse = await axios.get(
+          "./snippets/login_signup_snippet.html",
+          { responseType: "text" }
+        )
+        container.innerHTML = htmlResponse.data
+      }
+    } catch (e) {
+      console.error("Error loading logged in state : " + e)
     }
+  }
 
-    axiosUtils.sendPostRequest = function (URL, data, handleResponse, handleError){
-        axios.post(URL, data)
-        .then(response => {
-            handleResponse(response);
-        })
-        .catch(error => {
-            handleError(error);
-        });
+  axiosUtils.loadPageContent = async function (pageName) {
+    try {
+      const response = await axios.get(
+        "./snippets/" + pageName + "_snippet.html"
+      )
+      const mainContent = document.getElementById("main-content")
+      mainContent.innerHTML = response.data
+
+      document.getElementById("home-navBtn").classList.remove("active")
+      document.getElementById("reservations-navBtn").classList.remove("active")
+      document.getElementById("searched-navBtn").classList.remove("active")
+      document.getElementById("single_rest-navBtn").classList.remove("active")
+
+      switch (pageName) {
+        case "home":
+          document.getElementById("home-navBtn").classList.add("active")
+          break
+        case "reservations":
+          document.getElementById("reservations-navBtn").classList.add("active")
+          mainContent.classList.add("reservation-container")
+          break
+        case "searched":
+          document.getElementById("searched-navBtn").classList.add("active")
+          break
+        case "single_rest":
+          document.getElementById("single_rest-navBtn").classList.add("active")
+          mainContent.classList.add("single-rest-container")
+          break
+        case "base":
+          break
+        default:
+          alert("Something went wrong!")
+          break
+      }
+    } catch (error) {
+      console.error("Error loading page content : " + error)
     }
+  }
 
-    axiosUtils.sendPutRequest = function (URL, data, handleResponse, handleError){
-        axios.put(URL, data)
-        .then(response => {
-            handleResponse(response);
-        })
-        .catch(error => {
-            handleError(error);
-        });
-    }
-
-    axiosUtils.sendDeleteRequest = function (URL, handleResponse, handleError){
-        axios.delete(URL)
-        .then(response => {
-            handleResponse(response);
-        })
-        .catch(error => {
-            handleError(error);
-        });
-    }
-
-    axiosUtils.loadLoggedInState = async function (){
-        try {
-            const response = await axios.get('/'); // ??? 
-            const data = response.data;
-            
-            const container = document.getElementById('login-signup-container');
-            if(data.loggedIn){
-                const htmlResponse = await axios.get('./snippets/logged_in_snippet.html', {responseType: 'text'});
-                container.innerHTML = htmlResponse.data; 
-            } else {
-                const htmlResponse = await axios.get('./snippets/login_signup_snippet.html', {responseType: 'text'});
-                container.innerHTML = htmlResponse.data;
-            }
-
-        } catch (e) {
-            console.error('Error loading logged in state : '+ e);
-        }
-    }
-
-    axiosUtils.loadPageContent = async function (pageName){
-        try {
-            const response = await axios.get('./snippets/' + pageName + '_snippet.html');
-            const mainContent = document.getElementById('main-content');
-            mainContent.innerHTML = response.data;
-
-            document.getElementById('home-navBtn').classList.remove('active');
-            document.getElementById('reservations-navBtn').classList.remove('active');
-            document.getElementById('searched-navBtn').classList.remove('active');
-            document.getElementById('single_rest-navBtn').classList.remove('active');
-
-            switch (pageName) {
-                case 'home':
-                    document.getElementById('home-navBtn').classList.add('active');
-                    break;
-                case 'reservations':
-                    document.getElementById('reservations-navBtn').classList.add('active');
-                    mainContent.classList.add('reservation-container');
-                    break;
-                case 'searched':
-                    document.getElementById('searched-navBtn').classList.add('active');
-                    break;
-                case 'single_rest':
-                    document.getElementById('single_rest-navBtn').classList.add('active');
-                    mainContent.classList.add('single-rest-container');
-                    break;
-                case 'base':
-                    break;
-                default:
-                    alert('Something went wrong!');
-                    break;
-            }
-            
-        } catch (error) {
-            console.error('Error loading page content : '+ error);
-        }
-    }
-  
-    $global.$axiosUtils = axiosUtils;
-})(window);
+  $global.$axiosUtils = axiosUtils
+})(window)
