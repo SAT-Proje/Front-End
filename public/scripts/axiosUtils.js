bcc95488853dc061f5dea645a3898dcfac2119e7
Bora ILCI <borailci@outlook.com>
2024-05-08 21:15:08 +0300

    query

--- public/scripts/axiosUtils.js
+++ public/scripts/axiosUtils.js
@@ -1,4 +1,4 @@
-(function ($global) {
+;(function ($global) {
   var axiosUtils = {}
   axiosUtils.sendGetRequest = function (URL, handleResponse, handleError) {
     axios
@@ -76,74 +76,87 @@
     }
   }
 
- // get all restaurants
-  axiosUtils.getRestaurants = async function() {
+  // get all restaurants
+  axiosUtils.getRestaurants = async function () {
     const restaurants = await fetch("/restaurants", {
       method: "GET",
       headers: { "Content-Type": "application/json" }
-    });
-    const data = await restaurants.json();
-    console.log(data);
-    return data;
+    })
+    const data = await restaurants.json()
+    console.log(data)
+    return data
   }
-
 
   axiosUtils.loadPageContent = async function (pageName, restaurantId = null) {
     try {
       const response = await axios.get(
         "./snippets/" + pageName + "_snippet.html"
       )
-      
+
       const mainContent = document.getElementById("main-content")
       mainContent.innerHTML = response.data
 
       if (pageName == "searched" && restaurantId == null) {
-        const restaurants = await getRestaurants();
-        let counter = 0;
+        const restaurants = await axiosUtils.getRestaurants()
+        let counter = 0
 
-        let searchResults = document.getElementById("search-results-container");
-        searchResults.innerHTML = "";
+        let searchResults = document.getElementById("search-result-container")
+        console.log(searchResults)
+        searchResults.innerHTML = ""
 
         if (restaurants.length == 0) {
-          searchResults.innerHTML = "No restaurants found";
+          searchResults.innerHTML = "No restaurants found"
         } else {
           restaurants.forEach(restaurant => {
-            console.log(restaurant, " is getting processed");
-            const restaurantCard = document.createElement("div");
-            restaurantCard.classList.add("restaurant-card");
-            restaurantCard.classList.add("card");
-            restaurantCard.classList.add("p-0");
-            restaurantCard.classList.add("out-0");
-            restaurantCard.innerHTML = `
-            <img src="./`+ restaurant.id + `.jpg class="card-img-top" alt="`+ restaurant.id+`-image" />
+            console.log(restaurant, " is getting processed")
+            const restaurantCard = document.createElement("div")
+            restaurantCard.classList.add("restaurant-card")
+            restaurantCard.classList.add("card")
+            restaurantCard.classList.add("p-0")
+            restaurantCard.classList.add("out-0")
+            restaurantCard.innerHTML =
+              `
+            <img src="./` +
+              restaurant.id +
+              `.jpg class="card-img-top" alt="` +
+              restaurant.id +
+              `-image" />
             <div class="card-body">
-              <h5 class="card-title rest-name">`+restaurant.about.name+`</h5>
+              <h5 class="card-title rest-name">` +
+              restaurant.about.name +
+              `</h5>
               <p class="card-text rest-sm-info">
               
               </p>
               <p class="card-text rest-open-hours">10AM - 11PM</p>
-              <div class="card-text rest-rating" id="`+restaurant.id+`-rating-section">
+              <div class="card-text rest-rating" id="` +
+              restaurant.id +
+              `-rating-section">
                 
               </div>
             </div>
-            <button type="button" class="btn text-center" onclick="$axiosUtils.loadPageContent('single_rest',`+restaurant.id+`)">Reserve a place!</button>
-            `;
-            mainContent.innerHTML += restaurantCard;
-            let ratings = document.getElementById(restaurant.id + "-rating-section");
-            
-            const full_stars = Math.floor(restaurant.about.rating);
-            const half_stars = 5 - half_stars;
+            <button type="button" class="btn text-center" onclick="$axiosUtils.loadPageContent('single_rest',` +
+              restaurant.id +
+              `)">Reserve a place!</button>
+            `
+            mainContent.innerHTML += restaurantCard
+            let ratings = document.getElementById(
+              restaurant.id + "-rating-section"
+            )
+
+            const full_stars = Math.floor(restaurant.about.rating)
+            const half_stars = 5 - half_stars
             for (let i = 0; i < full_stars; i++) {
-              let star = document.createElement("i");
-              star.classList.add("fa-solid");
-              star.classList.add("fa-star");
-              ratings.appendChild(star);
-            };
+              let star = document.createElement("i")
+              star.classList.add("fa-solid")
+              star.classList.add("fa-star")
+              ratings.appendChild(star)
+            }
             for (let i = 0; i < half_stars; i++) {
-              let star = document.createElement("i");
-              star.classList.add("fa-regular");
-              star.classList.add("fa-star");
-              ratings.appendChild(star);
+              let star = document.createElement("i")
+              star.classList.add("fa-regular")
+              star.classList.add("fa-star")
+              ratings.appendChild(star)
             }
           })
         }
@@ -154,10 +167,6 @@
                 <i class="fa-solid fa-star"></i>
                 <p class="rest-rating-text">5.0</p>*/
       }
-
-
-
-      
 
       document.getElementById("home-navBtn").classList.remove("active")
       document.getElementById("reservations-navBtn").classList.remove("active")
